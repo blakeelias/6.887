b482188b104cfe6f7431ce23fb8f647f0a5567d2
Blake Elias <blakeelias@gmail.com>
2017-02-22 22:00:40 -0500

    Add code for pset 1

--- pset1/Pset1.v
+++ pset1/Pset1.v
@@ -2,14 +2,31 @@ Require Import Frap Pset1Sig.
 
 Theorem another_important_theorem : length [1; 2; 3] = 1 + length [4; 5].
 Proof.
-Admitted.
+simplify.
+equality.
+Qed.
 
 Theorem length_concat : forall A (xs ys : list A),
     length (xs ++ ys) = length xs + length ys.
 Proof.
-Admitted.
+
+ intros A.
+ induction xs.
+ intros ys.
+ simplify.
+ equality.
+ intros ys.
+ simplify.
+ rewrite IHxs.
+ equality.
+Qed. 
 
 Theorem length_rev : forall A (xs : list A),
     length xs = length (rev xs).
 Proof.
-Admitted.
+ induction xs; simpl; auto.
+ rewrite length_concat.
+ rewrite IHxs.
+ simplify.
+ intuition.
+Qed.
