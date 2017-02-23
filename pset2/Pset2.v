8714e853a5d6c94e9b8b85bf302c6ed67b6d19f1
Blake Elias <blakeelias@gmail.com>
2017-02-22 22:01:21 -0500

    Add draft code for pset 2

--- pset2/Pset2.v
+++ pset2/Pset2.v
@@ -8,7 +8,7 @@ Require Import OrdersFacts.
  *)
 Include (OrderedTypeFacts Pset2Sig).
 Include (OrderedTypeTest Pset2Sig).
-(* Print OrderedTypeFacts. *)
+Print OrderedTypeFacts.
 (* Print OrderedTypeTest. *)
 
 (* Our proof also uses the following facts that have been included above from libraries. *)
@@ -26,15 +26,113 @@ Check compare_eq_iff.
 
 Theorem insert_member: forall tr n, BST tr -> member n (insert n tr) = true.
 Proof.
-Admitted.
+induct tr.
+simplify.
+rewrite compare_refl.
+equality.
+simplify.
+cases (compare n t).
+simplify.
+rewrite Heq; equality.
+simplify.
+rewrite Heq.
+rewrite IHtr1; equality.
+simplify.
+rewrite Heq.
+rewrite IHtr2; equality.
+Qed.
+
+Lemma insert_less_than: forall tr n a, BST tr /\ tree_lt a tr /\ lt n a -> tree_lt a (insert n tr).
+Proof.
+induct tr.
+simplify.
+propositional.
+simplify.
+case (compare n t).
+simplify.
+propositional.
+simplify.
+propositional.
+apply IHtr1.
+propositional.
+simplify.
+propositional.
+apply IHtr2.
+propositional.
+Qed.
+
+Lemma insert_greater_than: forall tr n a, BST tr /\ tree_gt a tr /\ lt a n -> tree_gt a (insert n tr).
+induct tr.
+simplify.
+propositional.
+simplify.
+case (compare n t).
+simplify.
+propositional.
+simplify.
+propositional.
+apply IHtr1.
+propositional.
+simplify.
+propositional.
+apply IHtr2.
+propositional.
+Qed.
+
+
 
 Theorem insert_ok: forall tr n, BST tr -> BST (insert n tr).
 Proof.
-Admitted.
+(*intros n tr.
+simplify.
+case (insert tr n).
+simplify. equality.*)
+
+
+induct tr.
+simplify.
+propositional.
+simplify.
+cases (compare n t).
+simplify.
+propositional.
+simplify.
+propositional.
+apply IHtr1.
+apply H0.
+simplify.
+apply insert_less_than.
+propositional.
+apply compare_lt_iff.
+apply Heq.
+
+simplify.
+propositional.
+apply IHtr2.
+apply H1.
+apply insert_greater_than.
+propositional.
+apply compare_gt_iff.
+apply Heq.
+Qed.
+
+(* Lemma tree_max: forall tr n, tree_lt (rightmost tr) tr. *)
 
 Theorem delete_ok: forall tr n, BST tr -> BST (delete n tr).
 Proof.
-Admitted.
+(*induct tr.
+simplify. equality.
+simplify.
+cases (compare n t).
+cases (rightmost tr1).
+simplify.
+propositional.
+cases (delete_rightmost tr1).
+simplify. equality.
+simplify.
+propositional.
+apply H*)
+
 
 (* OPTIONAL PART! *)
 (*Theorem delete_member: forall tr n, BST tr -> member n (delete n tr) = false.
