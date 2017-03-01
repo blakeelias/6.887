241515d353f4d4714fa63d68317914cbeeb75098
Blake Elias <blakeelias@gmail.com>
2017-03-01 12:21:26 -0500

    Implement code for parents functions

--- pset3/Pset3Sig.v
+++ pset3/Pset3Sig.v
@@ -79,5 +79,38 @@ Fixpoint getNodesWithNodeName (treeList : list tree) (name : string) : list tree
 Fixpoint slashNodeName (treeList : list tree) (name : string) : list tree :=
   getNodesWithNodeName (childrenList treeList) name.
 
-Let n1 = NodeWithValue "number" "6.009".
+Fixpoint tree_eq (tr1 tr2 : tree) : bool :=
+  match tr1 with
+  | Leaf => 
+    match tr2 with
+    | Leaf => true
+    | NodeWithValue n v => false
+    | NodeWithChildren n tr1 tr2 => false
+    end
+  | NodeWithValue n1 v1 => match tr2 with
+    | Leaf => false
+    | NodeWithValue n2 v2 => andb (string_eq n1 n2) (string_eq v1 v2)
+    | NodeWithChildren n tr1 tr2 => false
+    end
+  | NodeWithChildren n1 tr1_1 tr1_2 => match tr2 with
+    | Leaf => false
+    | NodeWithValue n2 v2 => false
+    | NodeWithChildren n2 tr2_1 tr2_2 => (string_eq n1 n2) && (tree_eq tr1_1 tr2_1) && (tree_eq tr1_2 tr2_2)
+    end
+  end.
+
+Fixpoint isParent (child parent : tree) : bool :=
+  match parent with
+  | Leaf => false
+  | NodeWithValue name value => false
+  | NodeWithChildren n tr1 tr2 => orb (tree_eq child tr1) (tree_eq child tr2)
+  end.
+
+Fixpoint parents (root tr : tree) : list tree :=
+   filter (isParent tr) (descendents root).
+
+Fixpoint parentsList (root : tree) (treeList : list tree) : list tree :=
+  flat_map (parents root) treeList.
+
+(* Let n1 = NodeWithValue "number" "6.009". *)
 
