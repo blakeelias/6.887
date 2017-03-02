94664d57d013fc461b1658408fede2625a50f22d
Blake Elias <blakeelias@gmail.com>
2017-03-02 02:46:43 -0500

    Add parent poiters

--- pset3/Pset3Sig.v
+++ pset3/Pset3Sig.v
@@ -11,12 +11,9 @@ Require Import List.
 
 
 Inductive tree :=
-| Leaf: tree
-| NodeWithValue: string -> string -> tree
-| NodeWithChildren: string -> tree -> tree -> tree.
-
-(* Then a singleton is just a node without subtrees. *)
-Definition Singleton (n: string) (v: string) := NodeWithValue n v.
+| Leaf: list tree -> tree
+| NodeWithValue: string -> string -> list tree -> tree
+| NodeWithChildren: string -> tree -> tree -> list tree -> tree.
 
 Inductive xpath : Set :=
 | NodeName (name : string)
@@ -29,23 +26,23 @@ Definition selection := list tree.
 
 Fixpoint name (tr : tree) : option string := 
   match tr with
-  | Leaf => None
-  | NodeWithValue name value => Some name
-  | NodeWithChildren name tr1 tr2 => Some name
+  | Leaf parent => None
+  | NodeWithValue name value parent => Some name
+  | NodeWithChildren name tr1 tr2 parent => Some name
   end.
 
 Fixpoint value (tr : tree) : option string := 
   match tr with
-  | Leaf => None
-  | NodeWithValue name value => Some value
-  | NodeWithChildren name tr1 tr2 => None
+  | Leaf parent => None
+  | NodeWithValue name value parent => Some value
+  | NodeWithChildren name tr1 tr2 parent => None
   end.
 
 Fixpoint children (tr : tree) : list tree :=
   match tr with
-  | Leaf => nil
-  | NodeWithValue name value => nil
-  | NodeWithChildren name tr1 tr2 => tr1 :: ( cons tr2 nil )
+  | Leaf parent => nil
+  | NodeWithValue name value parent => nil
+  | NodeWithChildren name tr1 tr2 parent => tr1 :: ( cons tr2 nil )
 end.
 
 Fixpoint childrenList (trs : list tree) : list tree :=
@@ -53,9 +50,9 @@ Fixpoint childrenList (trs : list tree) : list tree :=
 
 Fixpoint descendents (tr : tree) : list tree :=
   match tr with
-  | Leaf => Leaf :: nil
-  | NodeWithValue name value => NodeWithValue name value :: nil
-  | NodeWithChildren name tr1 tr2 => (NodeWithChildren name tr1 tr2) :: (descendents tr1 ++ descendents tr2)
+  | Leaf _ => tr :: nil
+  | NodeWithValue _ _ _ => tr :: nil
+  | NodeWithChildren name tr1 tr2 parent => tr :: (descendents tr1 ++ descendents tr2)
   end. 
 
 Fixpoint descendentsList (trList : list tree) : list tree :=
@@ -68,9 +65,9 @@ Fixpoint string_eq (str1 str2 : string) : bool :=
 
 Fixpoint hasValue (val : string) (tr : tree) : bool :=
   match tr with
-  | Leaf => false
-  | NodeWithValue name value => string_eq val value
-  | NodeWithChildren name tr1 tr2 => false
+  | Leaf _ => false
+  | NodeWithValue name value _ => string_eq val value
+  | NodeWithChildren name tr1 tr2 _ => false
   end.
 
 Fixpoint getNodesWithValue (selectedTreeList : list tree) (val : string) : list tree :=
@@ -78,9 +75,9 @@ Fixpoint getNodesWithValue (selectedTreeList : list tree) (val : string) : list 
 
 Fixpoint hasName (name : string) (tr : tree) : bool :=
   match tr with
-  | Leaf => false
-  | NodeWithValue n v => string_eq n name
-  | NodeWithChildren n tr1 tr2 => string_eq n name
+  | Leaf _ => false
+  | NodeWithValue n v _ => string_eq n name
+  | NodeWithChildren n tr1 tr2 _ => string_eq n name
   end.
 
 Fixpoint getNodesWithNodeName (treeList : list tree) (name : string) : list tree :=
@@ -89,7 +86,7 @@ Fixpoint getNodesWithNodeName (treeList : list tree) (name : string) : list tree
 Fixpoint childrenWithNodeName (treeList : list tree) (name : string) : list tree :=
   getNodesWithNodeName (childrenList treeList) name.
 
-Fixpoint tree_eq (tr1 tr2 : tree) : bool :=
+(* Fixpoint tree_eq (tr1 tr2 : tree) : bool :=
   match tr1 with
   | Leaf => 
     match tr2 with
@@ -107,33 +104,37 @@ Fixpoint tree_eq (tr1 tr2 : tree) : bool :=
     | NodeWithValue n2 v2 => false
     | NodeWithChildren n2 tr2_1 tr2_2 => (string_eq n1 n2) && (tree_eq tr1_1 tr2_1) && (tree_eq tr1_2 tr2_2)
     end
-  end.
+  end. *)
 
-Fixpoint isParent (child parent : tree) : bool :=
+(* Fixpoint isParent (child parent : tree) : bool :=
   match parent with
   | Leaf => false
   | NodeWithValue name value => false
   | NodeWithChildren n tr1 tr2 => (orb (tree_eq child tr1) (tree_eq child tr2))
+  end. *)
+
+Fixpoint parent (tr : tree) : list tree :=
+  match tr with
+  | Leaf parent => parent
+  | NodeWithValue _ _ parent => parent
+  | NodeWithChildren _ _ _ parent => parent
   end.
 
-Fixpoint parents (root tr : tree) : list tree :=
-   filter (isParent tr) (descendents root).
-
-Fixpoint parentsList (root : tree) (treeList : list tree) : list tree :=
-  flat_map (parents root) treeList.
+Fixpoint parentsList (treeList : list tree) : list tree :=
+  flat_map parent treeList.
 
 (* Can try constructing a node as follows: 
 Let n1 = NodeWithValue "number" "6.009".
 *)
 
 
-Fixpoint interp (e : xpath) (s : selection) (root : tree) : selection :=
+Fixpoint interp (e : xpath) (s : selection) : selection :=
   match e with
   | NodeName n => childrenWithNodeName s n
   | Value v => getNodesWithValue s v
-  | Parents => parentsList root s
+  | Parents => parentsList s
   | Descendents => descendentsList s
-  | Concat e1 e2 => interp e2 (interp e1 s root) root
+  | Concat e1 e2 => interp e2 (interp e1 s)
   end.
 
 (** Begin proofs below **)
@@ -143,20 +144,23 @@ Require Import List.
 Require Import Frap.
 
 Theorem no_extra_children: forall root n1 n2 x,
-  In x (interp (Concat (NodeName n1)
-                (Concat (NodeName n2)
-                  Parents)) (cons root nil) root)
-  -> In x (interp (NodeName n1) (cons root nil) root).
+  In x (interp 
+  (Concat (NodeName n1)
+   (Concat (NodeName n2)
+    Parents)) (cons root nil))
+  -> In x (interp 
+    (NodeName n1)
+    (cons root nil)).
 Proof.
   simplify.
-  induction root.
-  simplify.
-  elim H.
-  equality.
-  simplify.
-  simplify.
 
-  
+
+(* another approach: 
+  induct root; simplify.
+  equality.
+  equality.
+  case root1.
+... unsure where to go next! *)
 Admitted.
 
 (* problem here... *)
